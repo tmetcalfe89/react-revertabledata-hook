a7d6319a0b5846fc925f10c5e602326d4ee60a65
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-05-03 15:47:25 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import { useCallback, useMemo, useState } from "react";
+
+export default function useRevertableData(defaultValues) {
+  const [newValues, setNewValues] = useState({});
+
+  const revert = useCallback(() => setNewValues({}), []);
+  const revertValue = useCallback(
+    (key) =>
+      setNewValues((prev) =>
+        JSON.parse(JSON.stringify({ ...prev, [key]: undefined }))
+      ),
+    []
+  );
+  const updateValue = useCallback(
+    (key, value) =>
+      setNewValues((prev) =>
+        defaultValues[key] === value
+          ? JSON.parse(JSON.stringify({ ...prev, [key]: undefined }))
+          : { ...prev, [key]: value }
+      ),
+    [defaultValues]
+  );
+  const isChanged = useCallback(
+    (key) => newValues[key] !== undefined,
+    [newValues]
+  );
+
+  const currentValues = useMemo(() => {
+    const retval = JSON.parse(JSON.stringify(defaultValues));
+    for (let key in newValues) {
+      retval[key] = newValues[key];
+    }
+    return retval;
+  }, [defaultValues, newValues]);
+  const anyUpdates = useMemo(
+    () => Object.keys(newValues).length !== 0,
+    [newValues]
+  );
+
+  const getValue = useCallback((key) => currentValues[key], [currentValues]);
+
+  return {
+    defaultValues,
+    currentValues,
+    newValues,
+    revert,
+    revertValue,
+    updateValue,
+    getValue,
+    isChanged,
+    anyUpdates,
+  };
+}
